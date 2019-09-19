0fc87bf1d6f8cced852bd8a828775954d9ce7ac6
jengu <jen.jennifer.gu@gmail.com>
2019-09-19 15:56:01 -0700

    testing pushing

--- student.hpp
+++ student.hpp
@@ -1,3 +1,11 @@
 //header file student.hpp to declare your classes
 using namespace std; //use namespace std
 #include <string> //you will have to use string in C++
+
+class Student
+{
+public:
+private:
+    string firstName;
+    string lastName;
+};
